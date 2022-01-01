fcf5bf0b71686081d9b2344c201f3b4d86a8ac71
Abrar Hasin Kamal <ahasin12@gmail.com>
2022-01-01 21:34:54 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -10,7 +10,6 @@ import cv2
 from tensorflow import keras
 import streamlit as st
 from PIL import Image 
-import io
 
 model=keras.models.load_model('model70')
 
@@ -18,8 +17,7 @@ test_image=st.file_uploader("Please upload an image showing a sign language: ", 
 
 
 def process_image(image):
-    img=Image.open(io.BytesIO(image))
-    img=cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+    img=cv2.imread(image, cv2.IMREAD_GRAYSCALE)
     img=cv2.resize(img, (200, 200))
     img=np.asarray(img)
     img=img.reshape(1,200,200,1)
@@ -28,6 +26,6 @@ def process_image(image):
     st.write(predict)
 #test_image='./Dataset/3_10p3_20170319_135524 32.jpg'
 if test_image is not None:
-    process_image(test_image)
+    process_image(test_image.name)
 else:
     st.write('Please reupload the file')
